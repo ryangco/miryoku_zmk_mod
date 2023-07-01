99ee21b90185236a1eaddf17907e9da4d606a817
ryangco <135596330+ryangco@users.noreply.github.com>
2023-07-02 07:16:19 +0800

    layermapping to layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -35,7 +35,7 @@
 #define MIRYOKU_CLIPBOARD_WIN
 #define MIRYOKU_EXTRA_QWERTY
 
-#define MIRYOKU_LAYERMAPPING_MEDIA \
+#define MIRYOKU_LAYER_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RGB_TOG,         C_BRIGHTNESS_DEC,         U_RGB_HUI,         U_RGB_SAI,         C_BRIGHTNESS_INC,         \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
