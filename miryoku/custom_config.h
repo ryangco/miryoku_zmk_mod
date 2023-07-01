034545d526f685cb525235752723433663860a69
ryangco <135596330+ryangco@users.noreply.github.com>
2023-07-02 07:07:04 +0800

    added layermapping to bottom

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -32,7 +32,11 @@
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
-
 #define MIRYOKU_CLIPBOARD_WIN
 #define MIRYOKU_EXTRA_QWERTY
+
+#define MIRYOKU_LAYERMAPPING_MEDIA \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RGB_TOG,         C_BRIGHTNESS_DEC,         U_RGB_HUI,         U_RGB_SAI,         C_BRIGHTNESS_INC,         \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
+U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
