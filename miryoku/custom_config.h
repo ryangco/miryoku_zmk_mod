be66083790cadfd16deb001ef8f60be28638a6cf
ryangco <135596330+ryangco@users.noreply.github.com>
2023-07-02 10:02:56 +0800

    LAYER MEDIA BOTTOM

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,17 +16,6 @@
 &kp PLUS  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp UNDER \
                K32  K33  K34       K35  K36  K37
 
-#define  MIRYOKU_LAYERMAPPING_MEDIA( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-&kp MINUS  K00  &kp C_BRIGHTNESS_DEC  K02  &kp C_BRIGHTNESS_INC  K04       K05  K06  K07  K08  K09  &kp PIPE \
-&kp ESC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp EQUAL \
-&kp PLUS  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp UNDER \
-               K32  K33  K34       K35  K36  K37
-
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTINNERINDEX 7 8
@@ -44,3 +33,9 @@
 
 #define MIRYOKU_CLIPBOARD_WIN
 #define MIRYOKU_EXTRA_QWERTY
+
+#define MIRYOKU_LAYER_MEDIA \
+U_RGB_HUI,         &kp C_BRIGHTNESS_DEC,         &kp C_VOL_UP,      &kp C_BRIGHTNESS_INC,         U_RGB_TOG,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+U_RGB_EFF,         &kp C_PREV,        &kp C_VOL_DN,      &kp C_NEXT,        U_EP_TOG,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       &u_out_tog,        U_NA,              &u_to_U_MEDIA,     &u_to_U_FUN,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp C_MUTE,        &kp C_PP,          &kp C_STOP,        U_NA,              U_NA,              U_NA,              U_NP,              U_NP
